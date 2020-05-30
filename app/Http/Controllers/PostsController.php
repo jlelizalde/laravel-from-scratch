2534f79727a5496c96e1bc3bc4cd4c87f69b46d6
Jose Luis Elizalde Rosales <jler.152@gmail.com>
2020-05-30 15:25:58 -0500

    Created a database (laravel7), configured database in .env file and store posts in table posts. Refactored controller and view accordingly. Now in the view we receive an object, not a string

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,19 +6,12 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post){
+    public function show($slug)
+    {
+        $post = \DB::table('posts')->where('slug', $slug)->first();
 
-        $posts = [
-            'my-first-post' => 'Hello, this is my first blog post!',
-            'my-second-post' => 'Now I am getting the hang of this blogging thing.'
-        ];
-            
-        if( !array_key_exists($post, $posts)) {
-            abort(404, 'Sorry, that post was not found.');
-        }
-            
         return view('post', [
-            'post' => $posts[$post]
+            'post' => $post
         ]);
     }
 }
