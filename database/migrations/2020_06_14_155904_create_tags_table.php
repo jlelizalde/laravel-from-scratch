6633a2c464baf2d8522954d706e5048c65b8aceb
Jose Luis Elizalde Rosales <jler.152@gmail.com>
2020-06-14 11:19:00 -0500

    Added new Tag model and migration. In the migration added code for linking table with all required constraints. Modified Article model to reflect the relationship with tags

---
+++ database/migrations/2020_06_14_155904_create_tags_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->timestamps();
+        });
+
+        // This creates the pivot/linking table
+        // The convention is to use the name of the related
+        // tables in singular, as this:
+        Schema::create('article_tag', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('article_id');
+            $table->unsignedBigInteger('tag_id');
+            $table->timestamps();
+            
+            // The combination between tag and article id must be unique
+            $table->unique(['article_id', 'tag_id']);
+
+            // Foreign keys
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+            $table->foreign('tag_id')->references('id')->on('tags')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+    }
+}
